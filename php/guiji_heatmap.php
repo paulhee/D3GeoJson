340635582cf99ff94103ff079ec1572c4b0b6b8e
paulhee <paulhee@163.com>
2018-06-05 10:36:03 +0800

    小改动

---
+++ php/guiji_heatmap.php
@@ -0,0 +1,38 @@
+<?php
+    date_default_timezone_set("Asia/Shanghai");
+    set_time_limit(0);
+    $tablename=$_GET["tablename"];//表名称
+    $enteredDay=$_GET["enteredDay"];
+    $mysqli = new mysqli("172.16.4.206:3306","root","admin","bigdata");
+    if(!$mysqli){
+        die('Could not connect:'.mysql_error());
+    }
+    $distinctIDsql = "select DISTINCT mac from " .$tablename ." where people='4' and enteredDay = '" .$enteredDay ."'";//查询全部数据
+    $result_ID = $mysqli->query($distinctIDsql);
+    //echo $distinctIDsql ."<br>";
+    $data = array();
+    $count=0;
+    while($row = $result_ID->fetch_assoc()){
+            $Onesql = "select * from " .$tablename ." where mac = '" .$row["mac"] ."' and enteredDay = '" .$enteredDay ."'";
+            //echo $Onesql ."<br>";
+            $result_One = $mysqli->query($Onesql);
+            $num_rows=mysqli_num_rows($result_One);
+            $jwd=array();
+            while($row1 = $result_One->fetch_assoc()){
+                $jwd[] = array(floatval($row1["jd"]),floatval($row1["wd"]),strtotime($row1["enteredTime"]));
+            }
+            if($num_rows == 1){
+                $jwd[] = $jwd[0];
+            }
+            //$color=array('normal'=>array('color'=>"rgba(200, 40, 0, 1)"));//"rgba(" .rand(1,255) ."," .rand(1,255) ."," .rand(1,255) .",1)"
+            $data[]=array('geo'=>$jwd,'count'=>"1");//'lonlat'=>$jwd,
+            //echo $Onesql ."<br>";
+            //if($count == 100){
+            //   break;
+            //}
+            //$count = $count+1;
+    }
+    $result_json = $data;//array('status'=>"success",'data' => $data)
+    $mysqli->close();
+    echo json_encode($result_json);
+?>
